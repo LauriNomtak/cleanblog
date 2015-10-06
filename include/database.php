0ebb86d258fb626574556ba02e72b12050d8c5ae
LauriNomtak <laurinomtak@gmail.com>
2015-10-07 00:19:27 +0300

    Update database.php

--- include/database.php
+++ include/database.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'config.php'
+require 'config.php';
 
 // Connect database
 $db = mysqli_connect('DATABASE_HOSTNAME', 'DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_DATABASE') or die(mysqli_error($db));
