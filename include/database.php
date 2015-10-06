e920474f51867fd17306f74cf74679682cf69214
LauriNomtak <laurinomtak@gmail.com>
2015-10-07 00:18:52 +0300

    Update database.php

--- include/database.php
+++ include/database.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'config.php'
+
 // Connect database
 $db = mysqli_connect('DATABASE_HOSTNAME', 'DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_DATABASE') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
